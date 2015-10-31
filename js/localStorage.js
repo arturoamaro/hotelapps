df7368f1334fe3c4592e6bff7cf3e7502e6f0acc
Manuel Amaro <astark@MacBook-Air-de-Arturo-2.local>
2015-10-31 11:17:49 -0600

    tercera clase funcion valida registro

---
+++ js/localStorage.js
@@ -0,0 +1,13 @@
+var ls = {
+    registroLocal: function(){
+        //agregar llave
+        window.localStorage.setItem("nom", device.uuid);
+        window.location.href = "#home";
+    },
+    estaRegistrado: function(){
+        var nom = window.localStorage.getItem("nom");
+        if(nom != undefined)
+            return true;
+        else return false;
+    }
+}
