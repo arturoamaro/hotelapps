1f9707d27ed86eab8578bfa6b9d3ca107d3bf113
Manuel Amaro <astark@MacBook-Air-de-Arturo-2.local>
2015-10-31 10:10:57 -0600

    tercera clase

--- js/API/fileTransfer.js
+++ js/API/fileTransfer.js
@@ -1,32 +1,22 @@
-var ft ={
-    win: function (r) {
-        if(r.response){
-            //EXITOSO
-            navigator.notification.alert(
-                'Registrado Correctamente',  // message
-                function(){
-                    navigator.vibrate(2000);
-                    navigator.notification.beep(2);
-                    window.localStorage.setItem("uuid",10);
-                    window.location.href = "#home";
-                },         // callback
-                'Bienvenido',            // title
-                'Registrado'                  // buttonName
-            );
-            
-        }else{
-            alert("Error");
-        }
-    },
-    fail: function (error) {
-        alert("error"+error.code);
-    },
-    start:function(path){
-        var options = new FileUploadOptions();
-        options.fileKey = "foto";
-        options.fileName = "AAA";//fileURL.substr(fileURL.lastIndexOf('/') + 1);
-        options.mimeType = "image/jpeg";
-        var ft = new FileTransfer();
-        ft.upload(path, encodeURI("http//carlos.igitsoft.com/apps/test.php"), ft.win, ft.fail, options);
-    }
-}
+var fileTransfer = {
+	win: function (r) {
+		if(r.response == '1'){
+			navigator.notification.alert("Los datos se han enviado satisfactoriamente", null, "Registro Correcto", "Aceptar");
+		}else{
+			alert("Error");
+		}
+			
+	},
+	fail: function (error) {
+		alert("An error has occurred: Code = " + error.code);
+	},
+	sendPhoto: function(path){
+		var options = new FileUploadOptions();
+		options.fileKey = "foto";
+		options.fileName = "Carlos";
+		options.mimeType = "image/jpeg";
+
+		var ft = new FileTransfer();
+		ft.upload(path, "http://carlos.igitsoft.com/apps/test.php", fileTransfer.win, fileTransfer.fail, options);
+	}
+};
--- js/actions.js
+++ js/actions.js
@@ -1,60 +1,29 @@
 var fn = {
-  init: function(){
-      //var x = false;
-      
-      if(!fn.estaRegistrado())
-          window.location.href = '#registro';
-      $("#regSend").click(fn.getReg);
-      $("#tomarFoto").click(mc.start);
-  },
-    deviceready:function(){
-        document.addEventListener("deviceready", fn.init, false);
-    },
-    estaRegistrado:function(){
-        if(window.localStorage.getItem('uuid') != undefined){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    ,
-    getReg:function (){
-        var nom = $("#regNom").val();
-        var tel = $("#regTel").val();
-        var mail = $("#regMail").val();
-        var foto = $("#tomarTomada").attr("rel");
-        //var nom = document.getElementById("regNom").value;
-        if(nom != "" &&tel != "" && mail !="" && foto!= undefined && foto!=""){//
-            //alert(nom +" - "+ tel +" - "+ mail);
-            fn.enviarRegistro(nom,mail,tel,foto);
-        }else{
-            //alert("Todos los campos son requeridos")
-            navigator.notification.alert('Todos los campos son requeridos');
-        }
-    },
-    enviarRegistro: function(nombre, mail, telefono, foto){
-        $.ajax({
-              method: "POST",
-              url: "http//carlos.igitsoft.com/apps/test.php",
-              data: { 
-                  nom: nombre, 
-                  mail: mail,
-                  tel: telefono
-              }
-        }).done(function( msg ) {
-            if(msg == 1){
-                //ENVIAR FOTO   
-                ft.start(foto);
-                
-            }else{
-                alert("datos incorrectos")
-            }
-        });
-    }
+	init: function(){
+		if(!fn.islogged())
+			window.location.href = "#reg";
+		
+		//Funcionalidades de Registro
+		$('#regSend').click(fn.getRegister);
+		$('#takePhoto').click(mediaCapture.takePhoto);
+	},
+	ready:function(){
+		document.addEventListener("deviceready", fn.init, false);
+	},
+	islogged: function(){
+		return false;
+	},
+	getRegister: function(){
+		var nom = $('#regName').val();
+		var tel = $('#regTel').val();
+		var mail = $('#regMail').val();
+		var foto = $('#regPhoto').attr('rel');
+		if(nom != '' && tel != '' && mail != '' && foto != undefined && foto != ''){
+			server.regSend(nom, tel, mail, foto);
+		}else{
+			navigator.notification.alert('Todos los campos son requeridos', null, "Error de Registro", "Aceptar");
+		}
+	}
 };
-//window.addEventListener("load",fn.init,false);
-//$(document).ready(fn.init);
-//comentar linea de abajo cuando la app este lista para compilar
-//$(fn.init)
-//descomentar la linea de abajo cuando la app este lista
-$(fn.deviceready);
+
+$(fn.ready);
