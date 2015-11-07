a124f9f82563e8ac828eb12e19d8aaba49ac9b6f
Manuel Amaro <astark@MacBook-Air-de-Arturo-2.local>
2015-11-07 11:29:55 -0600

    primeros 11 coros

--- js/actions.js
+++ js/actions.js
@@ -2,10 +2,13 @@ var fn = {
 	init: function(){
 		if(!fn.islogged())
 			window.location.href = "#reg";
-		
+		//window.location.href = "#nr1";
 		//Funcionalidades de Registro
 		$('#regSend').click(fn.getRegister);
 		$('#takePhoto').click(mediaCapture.takePhoto);
+        $('#nr1 ul[data-role = listview] a').click(fn.seleccionarTipo);
+        $('#nr1 div[data-role = navbar] li').click(fn.nr1Siguiente);
+        $('#nr2 div[data-role = navbar] li').click(fn.nr2Reservar);
 	},
 	ready:function(){
 		document.addEventListener("deviceready", fn.init, false);
@@ -23,7 +26,38 @@ var fn = {
 		}else{
 			navigator.notification.alert('Todos los campos son requeridos', null, "Error de Registro", "Aceptar");
 		}
-	}
+	},
+    seleccionarTipo: function(){
+        //console.log("aasdfasd");
+        //$('#nr1 ul[data-role = listview] a').removeClass("ui-btn-active");
+        $(this).parents("ul").find("a").removeClass("ui-btn-active");
+        $(this).addClass("ui-btn-active");
+        $("#nr1").attr("th", $(this).text());
+        
+    },
+    nr1Siguiente: function(){
+        if($(this).index()==1 && $("#nr1").attr("th")!= undefined){
+            window.location.href = "#nr2";
+        }else{
+            if($(this).index() != 0){
+                alert("Es necesario seleccionar un tipo de habitacion");
+            }
+        }
+    },
+    nr2Reservar: function(){
+        var th = $("#nr1").attr("th");
+        var np = $("#numPersonas").val();
+        var nh = $("#numHabitaciones").val();
+        var nd = $("#numDias").val();
+        if(conexion.estaConectado()){
+            server.envRes(th,np,nh,nd);
+        }else{
+            $.mobile.loading("show");
+            almacen.guardaReserva(th,np,nh,nd);
+            $.mobile.loading("hide");
+        }
+    }
 };
 
 $(fn.ready);
+//fn.init();
---
+++ js/reservas.js
@@ -0,0 +1,27 @@
+var almacen = {
+    db:null,
+    th:null,
+    np:null,
+    nh:null,
+    nd:null,
+    guardaReserva: function(th, np, nh, nd){
+        almacen.db = window.openDatabase("hotelApp", "1.0", "Hotel App", 20000);
+        almacen.th =th;
+        almacen.np =np;
+        almacen.nh =nh;
+        almacen.nd =nd;
+        almacen.db.transaction(almacen.tablaReserva, almacen.error, almacen.exito);
+    },
+    error: function(e){
+        alert("Error, codigo: "+e.code);
+    },
+    exito: function(){
+        alert("Reserva guardada en dispositivo, en espera de sincronizacion");
+        
+    },
+    tablaReserva: function(tx){
+        tx.executeSql("create table if not exists reservas(th, np, nh, nd)");
+        tx.executeSql(" insert into reservas (th,np,nh,nd) values("+almacen.th+","+almacen.np+","+almacen.nh+","+almacen.nd+")");
+    }
+    
+}
