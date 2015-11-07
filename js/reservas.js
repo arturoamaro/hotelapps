e83bd9e470f69b2e79bc59dfd94d3e6916aa5ac8
Manuel Amaro <astark@MacBook-Air-de-Arturo-2.local>
2015-11-07 12:17:20 -0600

    cuarta clase

--- js/reservas.js
+++ js/reservas.js
@@ -11,6 +11,8 @@ var almacen = {
         almacen.nh =nh;
         almacen.nd =nd;
         almacen.db.transaction(almacen.tablaReserva, almacen.error, almacen.exito);
+        almacen.db.transaction(almacen.tablaReserva2, almacen.error, almacen.exito);
+        alert("Reserva guardada en BD");
     },
     error: function(e){
         alert("Error, codigo: "+e.code);
@@ -19,9 +21,10 @@ var almacen = {
         alert("Reserva guardada en dispositivo, en espera de sincronizacion");
     },
     tablaReserva: function(tx){
-        tx.executeSql("create table if not exists reservas(th, np, nh, nd); ");
+        tx.executeSql("create table if not exists reservas(th, np, nh, nd) ");  
+    },
+    tablaReserva2: function(tx){
         tx.executeSql("insert into reservas(th,np,nh,nd) values("+almacen.th+","+almacen.np+","+almacen.nh+","+almacen.nd+"); ");
-        alert("Reserva guardada en BD");
     }
     
 }
